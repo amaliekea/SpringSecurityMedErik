1b4186ba012ac6030e858891720a2192633d970b
amaliekea <amje0001@stud.kea.dk>
2025-03-26 09:01:46 +0100

    init

---
+++ src/main/java/org/example/springsecurity11/controller/ContactController.java
@@ -0,0 +1,14 @@
+package org.example.springsecurity11.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class ContactController {
+
+    @GetMapping("/contact")
+    public String getContactDetails() {
+        return "Here are the contactdetails from db";
+    }
+}
---
+++ src/main/java/org/example/springsecurity11/controller/HelloWorld.java
@@ -0,0 +1,14 @@
+package org.example.springsecurity11.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @GetMapping("/test")
+    public String gettest() {
+        return "hello";
+    }
+}
